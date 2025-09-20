8ccf23d134bb0a32493182711690f590d427388c
Oshota <kakidesu1111@gmail.com>
2025-09-21 04:48:36 +0900

    Revet (#12)
    * revert1
    
    * Revert "list props (#8)"
    
    This reverts commit 71811da1a4ff97d0619378bd660cd1120989789f.

--- src/app/api/get-table/get-table.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const name = searchParams.get('name') || 'World';
-
-  return NextResponse.json({ message: `Hello, ${name} from Route Handler!` });
-}
-
