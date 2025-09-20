32c9f60aadcd7da72b87bc17f0105ab70f366894
Oshota501 <kakidesu1111@gmail.com>
2025-09-21 04:39:16 +0900

    revert1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import { auth } from "@/auth";
 import VoiceRecognition from "@/app/components/VoiceRecognition";
 import { User } from "@/type/index";
 
-import List from "@/app/components/SummaryListGet";
-
 export default async function Home() {
   const session = await auth();
   const user = session?.user as User;
