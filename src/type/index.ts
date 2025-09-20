ccbe3aaecf993966d391faab3470766cfe35dbe9
ikkei1616 <is0727xe@ed.ritumei.ac.jp>
2025-09-21 06:16:23 +0900

    feat:　型定義の追加

--- src/type/index.ts
+++ src/type/index.ts
@@ -4,3 +4,24 @@ export type User = {
   email?: string | null | undefined;
   image?: string | null | undefined;
 };
+
+export type ReadingLogList = {
+    id : number
+    title : string
+    user_id : number
+    summary : string
+    createdAt : string
+    updatedAt : string
+    deletedAt : string | null
+}
+
+export type ReadingLogListPage = {
+    logList : ReadingLogList[] ;
+}
+
+export type ReadingListCardProps = {
+    createdAt : string
+    updatedAt : string
+    title : string
+    summary : string
+}
