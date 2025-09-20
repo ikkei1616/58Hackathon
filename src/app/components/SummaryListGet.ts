1e175ae4b9a75e9165cb040c710f7d86f9aeb97b
Oshota501 <kakidesu1111@gmail.com>
2025-09-21 03:22:45 +0900

    Summary List get ref

--- src/app/components/SummaryListGet.ts
+++ src/app/components/SummaryListGet.ts
@@ -1,3 +1,21 @@
-export default function List () {
-    
+type UserIdProps = {
+    userId : string
 }
+export async function ListGet ({userId}:UserIdProps) {
+    try {
+      const res = await fetch(`http://localhost:3000/api/getAllSummary?authId=${userId}`, {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+      if (!res.ok) {
+        throw new Error("Failed to post summary");
+      }
+      const data = await res.json();
+      console.log(data);
+    } catch (e){
+      console.error(e);
+    }
+}
+export async SummaryList = async ListGet ();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { auth } from "@/auth";
 import VoiceRecognition from "@/app/components/VoiceRecognition";
 import { User } from "@/type/index";
 
+import List from "@/app/components/SummaryListGet";
+
 export default async function Home() {
   const session = await auth();
   const user = session?.user as User;
