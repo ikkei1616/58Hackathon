39e81859f80e756b3eb20ab7c2f69748eb26b3da
Oshota501 <kakidesu1111@gmail.com>
2025-09-21 02:23:09 +0900

    list props

---
+++ src/app/api/get-table/get-table.ts
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const name = searchParams.get('name') || 'World';
+
+  return NextResponse.json({ message: `Hello, ${name} from Route Handler!` });
+}
+
